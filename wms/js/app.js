918a884592986f62f3939039667a0c511184c86e
melbsurfer <melbsurfer@gmail.com>
2014-11-22 16:22:22 -0500

    Modified base map and Fly To javascript added
    Added the new base map data that is pointed to the PostGIS database on
    Geoserver.  Also added some eye candy for zooming from Baghdad to
    Tehran.

--- wms/js/app.js
+++ wms/js/app.js
@@ -1,6 +1,16 @@
 
 $(function(){
-	//console.log('firing!');
+	//console.log('firing!');33.311686
+	var baghdad = ol.proj.transform([44.355905,33.311686], 'EPSG:4326', 'EPSG:3857');
+	var tehran = ol.proj.transform([51.3498186,35.7014396], 'EPSG:4326', 'EPSG:3857');
+
+	var view = new ol.View({
+  		// the view's initial state
+  		center: baghdad,
+  		zoom: 8
+	});
+
+
 	var map = new ol.Map({
 	    target: 'map',
 	    renderer: 'canvas',
@@ -8,28 +18,56 @@ $(function(){
 	    //    	new ol.layer.Tile({
     	// 		source: new ol.source.MapQuest({layer: 'sat'})
   			// }),
-			new ol.layer.Image({
+			new ol.layer.Tile({
 			    opacity: 1.0,
-			    source: new ol.source.ImageWMS({
-					url: 'http://10.0.10.180:8080/geoserver/Natural_Earth_WS/wms',
-		      		params: {'LAYERS': 'Natural_Earth_LG'},
-			      	serverType: 'geoserver'
+			    source: new ol.source.TileWMS({
+					url: 'http://localhost:8080/geoserver/osm/wms',
+		      		params: {'LAYERS': 'omar-basemap_lg', 'TILED': true },
+			      	//serverType: 'geoserver'
     			})
  			 }),
-			new ol.layer.Image({
-			    opacity: 1.0,
-			    source: new ol.source.ImageWMS({
-					url: 'http://localhost:8080/geoserver/osm-test/wms',
-		      		params: {'LAYERS': 'osm-test:planet_osm_line'},
-			      	serverType: 'geoserver'
-    			})
- 			 })
+			// new ol.layer.Tile({
+			//     opacity: 1.0,
+			//     source: new ol.source.TileWMS({
+			// 		url: 'http://localhost:8080/geoserver/osm/wms',
+		 //      		params: {'LAYERS': 'planet_osm_line', 'TILED': true},
+			//       	//serverType: 'geoserver'
+   //  			})
+ 		// 	 }),
+			// new ol.layer.Image({
+			//     opacity: 1.0,
+			//     source: new ol.source.ImageWMS({
+			// 		url: 'http://localhost:8080/geoserver/osm/wms',
+		 //      		params: {'LAYERS': 'osm:planet_osm_line'},
+			//       	serverType: 'geoserver'
+   //  			})
+ 		// 	 })
 	    ],
-	    view: new ol.View({
-	      center: ol.proj.transform([44.355905, 33.311686], 'EPSG:4326', 'EPSG:3857'),
-	      zoom: 5
-	    })
+	    view: view //new ol.View({
+	      // center: ol.proj.transform([-80.6552775, 28.1174805], 'EPSG:4326', 'EPSG:3857'),
+	      // zoom: 15
+	       //center: ol.proj.transform([35.4954794, 33.8886284], 'EPSG:4326', 'EPSG:3857'),
+	      //zoom: 14
+	    //})
 	});
+
+	var fly = document.getElementById('fly');
+	fly.addEventListener('click', function() {
+  	var duration = 2000;
+  	var start = +new Date();
+  	var pan = ol.animation.pan({
+    	duration: duration,
+    	source: /** @type {ol.Coordinate} */ (view.getCenter()),
+    	start: start
+  	});
+  	var bounce = ol.animation.bounce({
+    	duration: duration,
+    	resolution: 4 * view.getResolution(),
+    	start: start
+  	});
+	map.beforeRender(pan, bounce);
+  		view.setCenter(tehran);
+	}, false);
 
 	//Full Screen
 	var myFullScreenControl = new ol.control.FullScreen();
